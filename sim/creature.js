e2bcb6de606f9e379682838e18f6fbcff0098fa6
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-30 17:12:27 -0500

    Convert indents from 4 spaces to 2 spaces (see style guide)

--- sim/creature.js
+++ sim/creature.js
@@ -7,18 +7,18 @@ var canMove=false;
 
 //Creature class
 function Creature() {
-    this.name = name;
-    this.speed = speed;
-    this.pos = pos;
+  this.name = name;
+  this.speed = speed;
+  this.pos = pos;
 }
 
 Creature.prototype.Move = function(){
-    if (canMove){
-        //move
-        }
-    else{
-        console.log("The creature cant move (bool canMove = false)");
+  if (canMove){
+    //move
     }
+  else{
+    console.log("The creature cant move (bool canMove = false)");
+  }
 };
 
 Creature.prototype.canIMoveWorld = function(){
@@ -26,9 +26,9 @@ Creature.prototype.canIMoveWorld = function(){
 };
 
 Creature.prototype.setCanMove = function(state){
-    this.canMove = state;
+  this.canMove = state;
 };
 
 Creature.prototype.Stop = function(){
-    
+  
 };
--- sim/world.js
+++ sim/world.js
@@ -1,16 +1,16 @@
 function World() {
-    this.size = 9001;
-    this.tiles = make2DArray(this.size);
+  this.size = 9001;
+  this.tiles = make2DArray(this.size);
 }
 
 //See http://stackoverflow.com/a/6495274/406249
 function make2DArray(size) {
-    size = size > 0 ? size : 0;
-    var arr = [];
+  size = size > 0 ? size : 0;
+  var arr = [];
 
-    while(size--) {
-        arr.push([]);
-    }
+  while(size--) {
+    arr.push([]);
+  }
 
-    return arr;
+  return arr;
 }
