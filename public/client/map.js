f6d7fa3b22f00e6ae2e3dfa779835f37d5a16735
seancongden <sean.congden@gmail.com>
2012-11-30 18:04:26 -0500

    Fix runtime

--- public/client/map.js
+++ public/client/map.js
@@ -2,12 +2,12 @@
 var Map = function Map(mapGroup) {
 	this.mapGroup = mapGroup;
 
-	this.tileGroup = new KineticGroup();
+	this.tileGroup = new Kinetic.Group();
 	mapGroup.add(this.tileGroup);
 }
 
 
-Map.prototype.loadTileData(tileData) {
+Map.prototype.loadTileData = function(tileData) {
 	this.tiles = tileData;
 
 	// Draw the tiles to the map layer
