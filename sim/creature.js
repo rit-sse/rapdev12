125dca445548eca1277e7b56bef2f92be341fa9b
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-30 16:54:00 -0500

    Add missing semicolons and a missing '}'

--- sim/creature.js
+++ sim/creature.js
@@ -21,21 +21,22 @@ Creature.prototype.Move = function(){
         //move
         }
     else{
-        console.log("The creature cant move (bool canMove = false)")
+        console.log("The creature cant move (bool canMove = false)");
     }
-}
+};
 
 Creature.prototype.canIMoveWorld = function(){
 
-}
+};
 
 
 Creature.prototype.setCanMove = function(state){
     this.canMove = state;
+};
 
 Creature.prototype.Stop = function(){
     
-}
+};
 
 
 
