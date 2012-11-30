d3d646e77540e3b65da10bf53b2d5e366f067f30
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-30 17:45:33 -0500

    Add semicolonrs to creature_core.js because Stacy is a hipster

--- sim/creature_core.js
+++ sim/creature_core.js
@@ -1,7 +1,7 @@
-var NORTH = [0, 1]
-var SOUTH = [0, -1]
-var EAST = [1, 0]
-var WEST = [-1, 0]
+var NORTH = [0, 1];
+var SOUTH = [0, -1];
+var EAST = [1, 0];
+var WEST = [-1, 0];
 
 function CreatureCore(x, y) {
   this.x = x;
