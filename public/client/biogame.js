3976301130489528ded7030ddb299a7ae2eed368
seancongden <sean.congden@gmail.com>
2012-11-30 16:45:34 -0500

    Add a canvas element and client js entry point

---
+++ public/client/biogame.js
@@ -0,0 +1,18 @@
+
+/*
+ * Create a websocket to the server and request
+ * the game state when loading the window.
+ */
+window.onload = function() {
+	//TODO: send websockets request to server API
+	//  to get the initail game state (send to initGame)
+}
+
+
+/*
+ * Client game entry point. Initializes game resources and loads
+ * the initial game world with data from the server.
+ */
+function initGame(data) {
+
+}
