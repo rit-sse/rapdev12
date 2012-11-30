390772762e57f85504293abbb8b08574cb1bf740
Kocsen Chung <kocsenc@gmail.com>
2012-11-30 16:45:39 -0500

    Start work on Creature class(creature.js)

---
+++ sim/creature.js
@@ -0,0 +1,44 @@
+//Creature.js
+
+var name;
+var speed;
+var pos;
+var canMove=false;
+
+
+
+//Creature class
+function Creature() {
+    this.name = name;
+    this.speed = speed;
+    this.pos = pos;
+
+}
+
+
+Creature.prototype.Move = function(){
+    if (canMove){
+        //move
+        }
+    else{
+        console.log("The creature cant move (bool canMove = false)")
+    }
+}
+
+Creature.prototype.canIMoveWorld = function(){
+
+}
+
+
+Creature.prototype.setCanMove = function(state){
+    this.canMove = state;
+
+Creature.prototype.Stop = function(){
+    
+}
+
+
+
+
+
+
