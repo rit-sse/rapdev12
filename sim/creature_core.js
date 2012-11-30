3950c9a5e6b51bf6e2e1299df61dc0f76ceffaca
Stacy Carlos <sjcarlos212@gmail.com>
2012-11-30 17:31:35 -0500

    Create creature core

---
+++ sim/creature_core.js
@@ -0,0 +1,13 @@
+var NORTH = [0, 1]
+var SOUTH = [0, -1]
+var EAST = [1, 0]
+var WEST = [-1, 0]
+
+function CreatureCore(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+CreatureCore.prototype.move = function(direction){
+  
+};
