eb6bf6ffaafbaaed9953dc0c881e8ac12628201a
mrm9084 <mrm9084@rit.edu>
2012-11-30 21:51:37 -0500

    basic creature functions

--- creature/Creature.js
+++ creature/Creature.js
@@ -1,1 +1,40 @@
 //The Creature API
+
+function move(direction){
+}
+
+function sprint(direction){
+}
+
+function attack(direction){
+}
+
+function lookAround(){
+}
+
+function sleepFor(time){
+}
+
+function act(){
+}
+
+function onBirth(){
+}
+
+function onCollision(object){
+}
+
+function onHit(direction){
+}
+
+function onWakeUp(reason){
+}
+
+function onDeath(){
+}
+
+function onSleepTurn(){
+}
+
+function onNoEnergy(){
+}
