e3cf4e3101aaad3fa573c67e85e7e495d4732d00
Jesse Jurman <j.r.jurman@gmail.com>
2012-11-30 19:31:04 -0500

    Created Method Stubs for World.js

---
+++ world/world.js
@@ -0,0 +1,29 @@
+var world = {
+	init:function(){
+		
+	},
+	
+	addCreature:function(creature){
+		
+	},
+	
+	populateWithItems:function(){
+		
+	},
+	
+	getTile:function(x, y){
+		
+	},
+	
+	getTerrainAtTile:function(x, y){
+		
+	},
+	
+	getInhabitantAtTile:function(x, y){
+		
+	},
+	
+	getItemAtTile:function(x, y){
+		
+	}
+};
