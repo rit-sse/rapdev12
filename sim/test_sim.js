59a4ae443615d11e7f7a8b9b5e5d03c59e12d403
Chris Bentivenga <chris.bentivenga@me.com>
2012-11-30 19:22:23 -0500

    Updated test_sim with epectations

--- sim/test_sim.js
+++ sim/test_sim.js
@@ -1,3 +1,12 @@
-var c_class = require("./creature_class");
+var c_class = require("./cricle_creature");
 
-var c1 = c_class.CreatureClass();
+var c1 = c_class.CircleCreature();
+var c2 = c_class.CircleCreature();
+
+for (var i=0; i<10; i++){
+	console.log("Turn %d", i)
+	c1.act();
+	c2.act();
+}
+
+console.log("End Simulation");
