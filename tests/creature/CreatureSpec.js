9ad4b527f0dcc1e8a77e09864e09ffbb475b824a
Steven Greenberg <sjg8961.edu>
2012-11-30 23:55:47 -0500

    Added secound creature to test

--- tests/creature/CreatureSpec.js
+++ tests/creature/CreatureSpec.js
@@ -1,9 +1,12 @@
-describe("Make a creature", function()){
-	var creature;
+describe("Make a creature", function(){
+	var creature1;
+	var creature2;
 	beforeEach(function() {
-		creature = new Creature();
+		creature1 = new Creature();
+		creature2 = new Creature();
 	});
-	it('calls onDeath', function(){
-		expect('You Died').toEqual(creature.onDeath());
+	it('calls onDeath',function(){
+		expect('You Died').toEqual(creature1.onDeath());
 	});
 });
+
