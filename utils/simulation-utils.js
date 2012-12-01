fe5247a27d64dc60742216aa2ee76a237e03e7c9
Steven Brunwasser <sbrunwasser@gmail.com>
2012-11-30 21:59:59 -0500

    added the partial directions to the enum

--- utils/simulation-utils.js
+++ utils/simulation-utils.js
@@ -4,7 +4,11 @@
  */
 var Direction = {
     NORTH: 0,
-    EAST: 1,
-    SOUTH: 2,
-    WEST: 3
+    NORTHEAST: 1,
+    EAST: 2,
+    SOUTHEAST: 3,
+    SOUTH: 4,
+    SOUTHWEST: 5,
+    WEST: 6,
+    NORTHWEST: 7
 };
