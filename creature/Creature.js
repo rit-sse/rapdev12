0d1ef9571884d3972ecef9f3b21ed7fe1ca2857b
mok4ry <mxm6060@rit.edu>
2012-12-02 15:53:45 -0500

    Added some logging in the Creature superclass

--- creature/Creature.js
+++ creature/Creature.js
@@ -67,24 +67,28 @@ Creature.prototype.act = function(){
 A User defined method of what there creature should do when they collide with an impassable object. The user will be given an identification of what the object is. If the object is another creature it will be the id of the creature. If it is a rock, water or the grid boarder then they will be given a string with its name. On collision is the only event that can be called any number of times.
 */
 Creature.prototype.onCollision = function(object){
+  console.log( this.name + ".onCollision called." );
 };
 
 /*
 A User defined method of what you creature will do when they get hit by another creature. They will also be given the direction that they need to attack for them to attack the creature. 
 */
 Creature.prototype.onHit = function(direction){
+  console.log( this.name + ".onHit called." );
 };
 
 /*
 A user defined method that defines what a user's creature should do on wakeup. They are also given a reason. If the reason is attacked onHit will be called as a second event after onWakeUp.
 */
 Creature.prototype.onWakeUp = function(reason){
+  console.log( this.name + ".onWakeUp called." );
 };
 
 /*
 A user defined method that will let the user let out one last scream before they die.
 */
 Creature.prototype.onDeath = function(){
+  console.log( this.name + ".onDeath called." );
 	return 'You Died';
 };
 
@@ -92,17 +96,19 @@ Creature.prototype.onDeath = function(){
 An API defined event that counts down to when your creature sould wake up or wakes you up if your creature gets hungry.
 */
 Creature.prototype.onSleepTurn = function(){
-    if (this.timeLeftToSleep > 0) {
-        this.timeLeftToSleep -= 1;
-    } else {
-        this.act();
-    }
+  console.log( this.name + ".onSleepTurn called." );
+  if (this.timeLeftToSleep > 0) {
+      this.timeLeftToSleep -= 1;
+  } else {
+      this.act();
+  }
 };
 
 /*
 Will make your creature pass out for the rest of the turn.
 */
 Creature.prototype.onNoEnergy = function(){
+  console.log( this.name + ".onNoEnergy called." );
 	this.passedOut = true;
 };
 
