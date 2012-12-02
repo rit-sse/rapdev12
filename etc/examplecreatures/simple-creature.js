566f9a79c5b3247735f9eb723706810ff543abff
Chris Bentivenga <chris.bentivenga@me.com>
2012-12-01 19:57:19 -0500

    updates the simple for more simple creatures

--- etc/examplecreatures/simple-creature.js
+++ etc/examplecreatures/simple-creature.js
@@ -5,10 +5,11 @@ var world = require(require('path').join(require('path').dirname(require.main.fi
 
 
 var monster = new Creature("my_name", 1, world, 100, 100, 100);
-
+var next_dir = Direction.SOUTH;
 monster.act = function() {
 	console.log("RAWR");
-    this.move(Direction.NORTH);
+    this.move(next_dir);
+    next_dir = (next_dir == Direction.NORTH) ? Direction.SOUTH : Direction.NORTH;
 };
 
 
